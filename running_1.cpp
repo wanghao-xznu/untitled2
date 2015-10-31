0c7f72ee1d5c71094be0d2d8cfc24697b0fdeb77
Howie_wang <375048164@qq.com>
2015-10-31 21:22:38 +0800

    howie seems shiyan 1 OK , send to donghailiang have a try

--- running_1.cpp
+++ running_1.cpp
@@ -244,6 +244,14 @@ void Running_1::onTimerOut()
 
                         }
                     }
+                    if((temp_power.toInt()>=arr_setting[0][0].toInt())&&flag_00)
+                    {
+                        flag_00 = 0;
+                        reach_time = 60*arr_setting[0][1].toInt() + arr_setting[0][2].toInt() - temp11*60 - temp12;
+                        arr_setting[0][4] = QString::number(reach_time/60);
+                        arr_setting[0][5] = QString::number(reach_time%60);
+                        Save_config_file("bilang.cfg");
+                    }
                     break;
         case 2:
                     if(temp22!=0){
@@ -266,6 +274,14 @@ void Running_1::onTimerOut()
                             //send duanwei 3 cmd
                             on_pushButton_clicked();
                         }
+                    }
+                    if((temp_power.toInt()>=arr_setting[1][0].toInt())&&flag_10)
+                    {
+                        flag_10 = 0;
+                        reach_time = 60*arr_setting[1][1].toInt() + arr_setting[1][2].toInt() - temp21*60 - temp22;
+                        arr_setting[1][4] = QString::number(reach_time/60);
+                        arr_setting[1][5] = QString::number(reach_time%60);
+                        Save_config_file("bilang.cfg");
                     }
                     break;
         case 3:
@@ -290,6 +306,14 @@ void Running_1::onTimerOut()
                             on_pushButton_clicked();
                         }
                     }
+                    if((temp_power.toInt()>=arr_setting[2][0].toInt())&&flag_20)
+                    {
+                        flag_20 = 0;
+                        reach_time = 60*arr_setting[2][1].toInt() + arr_setting[2][2].toInt() - temp31*60 - temp32;
+                        arr_setting[2][4] = QString::number(reach_time/60);
+                        arr_setting[2][5] = QString::number(reach_time%60);
+                        Save_config_file("bilang.cfg");
+                    }
                     break;
         case 4:
                     if(temp42!=0){
@@ -313,6 +337,14 @@ void Running_1::onTimerOut()
                             on_pushButton_clicked();
                         }
                     }
+                    if((temp_power.toInt()>=arr_setting[3][0].toInt())&&flag_30)
+                    {
+                        flag_30 = 0;
+                        reach_time = 60*arr_setting[3][1].toInt() + arr_setting[3][2].toInt() - temp41*60 - temp42;
+                        arr_setting[3][4] = QString::number(reach_time/60);
+                        arr_setting[3][5] = QString::number(reach_time%60);
+                        Save_config_file("bilang.cfg");
+                    }
                     break;
         case 5:
                     if(temp52!=0){
@@ -334,6 +366,14 @@ void Running_1::onTimerOut()
                             timer->stop();
                             qDebug()<<"Game over";
                         }
+                    }
+                    if((temp_power.toInt()>=arr_setting[4][0].toInt())&&flag_40)
+                    {
+                        flag_40 = 0;
+                        reach_time = 60*arr_setting[4][1].toInt() + arr_setting[4][2].toInt() - temp51*60 - temp52;
+                        arr_setting[4][4] = QString::number(reach_time/60);
+                        arr_setting[4][5] = QString::number(reach_time%60);
+                        Save_config_file("bilang.cfg");
                     }
                     break;
     default:
